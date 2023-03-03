329143a37567014a26b754bc8b6dff0a5ce0cd4e
m.iwamoto <>
2023-03-03 11:35:42 +0900

    config.yml

--- tax-api-experiment/jest.config.ts
+++ tax-api-experiment/jest.config.ts
@@ -1,3 +1,10 @@
 export default {
   preset: 'ts-jest/presets/js-with-ts-esm',
+  reporters: [
+    'default',
+    [
+      'jest-junit',
+      { outputDirectory: 'reports/jest', outputName: 'jest-report.xml' },
+    ],
+  ],
 }
